44e93c1f2fcdb9e1fd8cc4e2a5eb6478262f84ed
Quoc Tran <Quoc.Tran@sioux.asia>
2019-06-24 14:58:57 +0700

    wip(deployment): set up backup

--- DEPLOYMENT/env/dev/publish_api.sh
+++ DEPLOYMENT/env/dev/publish_api.sh
@@ -3,13 +3,11 @@ DEPLOY_ENVIRONMENT=dev
 CURRENT_PATH=$(pwd)
 TARGET_PATH="../../../API/API"
 OUTPUT_PATH="${CURRENT_PATH}/build_output/api"
-REMOTE_PATH						=/home/ubuntu/services/projects/giveandtake/Trunk/deployment/deployment/env/${DEPLOY_ENVIRONMENT}/output/api
-DEPLOYMENT_REMOTE_PATH=/home/ubuntu/services/projects/giveandtake/Trunk/deployment/deployment/env/${DEPLOY_ENVIRONMENT}
-REMOTE_ID=ubuntu@103.101.76.127
+DEPLOYMENT_REMOTE_PATH=~/project-skeleton/DEPLOYMENT/env/dev
+REMOTE_ID=quoctran@192.168.164.134
 
 echo "THIS WILL DEPLOY ON: ${DEPLOY_ENVIRONMENT}"
-if [ -d "${TARGET_PATH}" ]; 
-then
+if [ -d "${TARGET_PATH}" ]; then
 	cd "${TARGET_PATH}"
 else
 	echo "target path not found"
@@ -29,23 +27,22 @@ cp -f * "${OUTPUT_PATH}"
 
 cd "${CURRENT_PATH}"
 
-docker-compose up -d --build --force-recreate
+# docker-compose up -d --build --force-recreate
 
 # docker build -t base-api .
 
 # docker run --rm --name base-api -p 8000:80 base-api:latest
 
-# read -r -p "Sending build to server and restart service now? [y/N] " response
-# case "$response" in
-#     [yY][eE][sS]|[yY])
-#         echo "SENDING PACKAGE ..."
-#         scp -C -i ./key/id_rsa -P 22 -r build_output/api/app ${REMOTE_ID}:${REMOTE_PATH}
-
-# 		echo "RESTART DOCKER ..."
-# 		export REMOTE_DOCKER_COMMAND="cd ${DEPLOYMENT_REMOTE_PATH};docker-compose up -d --force-recreate giveandtake-api-dev;"
-# 		ssh -i ./key/id_rsa -p 22 ${REMOTE_ID} ${REMOTE_DOCKER_COMMAND}
-#         ;;
-#     *)
-#         echo "not sending data, service not restarted. Exiting ..."
-#         ;;
-# esac
+read -r -p "Sending build to server and restart service now? [y/N] " response
+case "$response" in
+[yY][eE][sS] | [yY])
+	echo "SENDING PACKAGE ..."
+	scp -C -i ./key/mykey -P 22 -r build_output/api ${REMOTE_ID}:~/volumes/${DEPLOY_ENVIRONMENT}
+	echo "RESTART DOCKER ..."
+	export REMOTE_DOCKER_COMMAND="cd ${DEPLOYMENT_REMOTE_PATH};docker-compose up -d --force-recreate base-api"
+	ssh -i ./key/mykey -p 22 ${REMOTE_ID} ${REMOTE_DOCKER_COMMAND}
+	;;
+*)
+	echo "not sending data, service not restarted. Exiting ..."
+	;;
+esac
