6dc91c4d51966caeccdaa9e1486db2af09d0f7c0
Quoc Tran <Quoc.Tran@sioux.asia>
2019-07-15 17:19:13 +0700

    wip(BookDetail): refactor book detail header

--- APP/src/components/BookDetailHeader/index.js
+++ APP/src/components/BookDetailHeader/index.js
@@ -1,84 +1,39 @@
 import {
   View,
-  Text,
   ImageBackground,
   Dimensions,
-  TouchableOpacity,
-  Image,
   StyleSheet,
   Animated
 } from 'react-native';
-import faker from 'faker';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Header } from 'react-navigation';
 import images from '../../helpers/imageHelper';
-import Rating from '../Rating';
-import colors from '../../helpers/colorHelper';
-
-export const MinimizedHeader = backPressed => (
-  <View style={styles.header}>
-    <TouchableOpacity>
-      <Image style={styles.back} source={images.back} resizeMode="stretch" />
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Image style={styles.back} source={images.download} resizeMode="stretch" />
-    </TouchableOpacity>
-  </View>
-);
-
-const BookInformation = () => (
-  <View style={styles.bookDetailContainer}>
-    <View style={styles.bookImageContainer}>
-      <Image source={images.book} style={styles.bookImage} />
-    </View>
-    <View style={styles.rightPanel}>
-      <View>
-        <Text style={styles.bookTitle} numberOfLines={2}>Tôi thấy hoa vàng trên cỏ xanh</Text>
-      </View>
-      <View>
-        <Text style={styles.normalText} numberOfLines={1}>Tác giả: {faker.random.words(2)}</Text>
-        <Text style={styles.normalText} numberOfLines={1}>Trạng thái: {faker.random.words(2)}</Text>
-        <Text style={styles.normalText} numberOfLines={1}>Số chương: {faker.random.number(100, 1000)}</Text>
-      </View>
-      <Rating rate={4.0} />
-
-      <View style={styles.buttons}>
-        <TouchableOpacity style={styles.readButtonContainer}>
-          <Text style={styles.readButton}>ĐỌC NGAY</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.roundButtonContainer}>
-          <Image source={images.bookmark} style={styles.roundButton} resizeMode="stretch" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.roundButtonContainer}>
-          <Image source={images.listening} style={styles.roundButton} resizeMode="stretch" />
-        </TouchableOpacity>
-      </View>
-    </View>
-  </View>
-);
+import MinimizedHeader from './MinimizedHeader';
+import BookInformation from './BookInformation';
 
 class BookDetailHeader extends Component {
   render() {
-    const { translateY } = this.props;
+    const { translateY, headerCoverHeight } = this.props;
     return (
-      <Animated.View style={styles.container}>
+      <Animated.View style={[styles.container, { height: headerCoverHeight }]}>
         <ImageBackground style={styles.headerBackground} source={images.book} resizeMode="cover">
           <View style={styles.headerBackgroundOverlay} />
         </ImageBackground>
-        <BookInformation />
+        <MinimizedHeader />
+        <BookInformation translateY={translateY} />
       </Animated.View>
     );
   }
 }
-const { width, height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     width,
-    height: 250,
+    height: 255,
     backgroundColor: 'white',
-    marginBottom: 5,
+    paddingBottom: 5,
+    overflow: 'hidden',
   },
   headerBackground: {
     width,
@@ -88,100 +43,6 @@ const styles = StyleSheet.create({
   headerBackgroundOverlay: {
     flex: 1,
     backgroundColor: '#00000088'
-  },
-  bookDetailContainer: {
-    flexDirection: 'row',
-    flex: 1,
-    marginTop: Header.HEIGHT
-  },
-  header: {
-    height: Header.HEIGHT,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0
-  },
-  back: {
-    width: 24,
-    height: 24,
-    tintColor: 'white',
-    marginLeft: 10,
-    marginRight: 10
-  },
-  bookImageContainer: {
-    width: 135,
-    height: 250 - Header.HEIGHT,
-    marginLeft: 15,
-    shadowColor: 'pink',
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 20,
-    shadowOpacity: 10,
-  },
-  bookImage: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 5,
-  },
-  rightPanel: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    flexDirection: 'column',
-    justifyContent: 'space-between'
-  },
-  bookTitle: {
-    color: 'white',
-    fontSize: 19,
-    fontWeight: '500',
-  },
-  normalText: {
-    color: 'white',
-    paddingTop: 3,
-    paddingBottom: 3
-  },
-  buttons: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
-  roundButtonContainer: {
-    width: 40,
-    height: 40,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: 'rgba(0,0,0, .4)',
-    shadowOffset: { height: 1, width: 1 },
-    shadowOpacity: 1,
-    shadowRadius: 1,
-    elevation: 2
-  },
-  roundButton: {
-    width: '60%',
-    height: '60%',
-    tintColor: colors.pink
-  },
-  readButtonContainer: {
-    height: 40,
-    backgroundColor: colors.pink,
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: 'rgba(0,0,0, .4)',
-    shadowOffset: { height: 1, width: 1 },
-    shadowOpacity: 1,
-    shadowRadius: 1,
-    elevation: 2
-  },
-  readButton: {
-    paddingLeft: 15,
-    paddingRight: 15,
-    color: 'white',
-    fontWeight: '500'
   }
 });
 
--- APP/src/screens/BookDetailScreen/index.js
+++ APP/src/screens/BookDetailScreen/index.js
@@ -8,10 +8,9 @@ import {
   View,
 } from 'react-native';
 import { TabBar, TabView } from 'react-native-tab-view'; // Version can be specified in package.json
-import { Header, ScrollView } from 'react-navigation';
+import { Header } from 'react-navigation';
 import faker from 'faker';
-import ContactsList from './ContactsList';
-import BookDetailHeader, { MinimizedHeader } from '../../components/BookDetailHeader';
+import BookDetailHeader from '../../components/BookDetailHeader';
 import Catalog from '../../components/Catalog';
 import HorizontalBookList from '../../components/HorizontalBookList';
 
@@ -20,8 +19,8 @@ const initialLayout = {
   width: Dimensions.get('window').width,
 };
 
-const TAB_BAR_HEIGHT = 52;
-const HEADER_MAX_HEIGHT = 255 + TAB_BAR_HEIGHT;
+const TAB_BAR_HEIGHT = 50;
+const HEADER_MAX_HEIGHT = 250 + TAB_BAR_HEIGHT;
 const HEADER_MIN_HEIGHT = Header.HEIGHT + TAB_BAR_HEIGHT;
 const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
@@ -36,21 +35,21 @@ class BookDetailScreen extends Component {
     this.state = {
       index: 0,
       routes: [
-        { key: '1', title: 'First' },
-        { key: '2', title: 'Second' },
-        { key: '3', title: 'Third' },
+        { key: '1', title: 'Tổng quan' },
+        { key: '2', title: 'Đánh giá' },
+        { key: '3', title: 'Bình luận' },
       ],
       scrollY: new Animated.Value(0),
     };
   }
 
-  _handleIndexChange = (index) => {
+  handleIndexChange = (index) => {
     this.setState({ index });
   };
 
   backPressed = () => {}
 
-  _renderHeader = (props) => {
+  renderHeader = (props) => {
     const headerHeight = this.state.scrollY.interpolate({
       inputRange: [0, HEADER_SCROLL_DISTANCE],
       outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
@@ -59,42 +58,51 @@ class BookDetailScreen extends Component {
 
     const translateY = this.state.scrollY.interpolate({
       inputRange: [0, HEADER_SCROLL_DISTANCE],
-      outputRange: [0, -HEADER_SCROLL_DISTANCE],
+      outputRange: [0, -HEADER_MAX_HEIGHT],
+      extrapolate: 'clamp',
+    });
+
+    const headerCoverHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [255, Header.HEIGHT],
       extrapolate: 'clamp',
     });
 
     return (
-      <Animated.View style={[styles.header, { transform: [{ translateY }] }]}>
-        <BookDetailHeader translateY={translateY} backPressed={this.props.backPressed} />
+      <Animated.View style={[styles.header, { height: headerHeight }]}>
+        <BookDetailHeader translateY={translateY} headerCoverHeight={headerCoverHeight} backPressed={this.props.backPressed} />
         <TabBar {...props} style={styles.tabBar} labelStyle={styles.tabBarLabel} />
       </Animated.View>
     );
   };
 
-  _renderScene = () => (
-    <Animated.ScrollView
-      style={{ paddingTop: HEADER_MAX_HEIGHT }}
-      scrollEventThrottle={16}
-      onScroll={Animated.event(
-        [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
-        { useNativeDriver: true }
-      )}
-    >
-      <Text style={styles.description} numberOfLines={10}>{faker.random.words(200)}</Text>
-      <Catalog onViewMore={this.showFullCatalog} />
-      <HorizontalBookList title="Cùng tác giả" />
-      <View style={{ height: HEADER_MAX_HEIGHT }} />
-    </Animated.ScrollView>
-  );
+  renderScene = () => {
+    const onScrollAnimating = Animated.event(
+      [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }]
+    );
+
+    return (
+      <Animated.ScrollView
+        style={{ paddingTop: HEADER_MAX_HEIGHT }}
+        scrollEventThrottle={16}
+        onScroll={onScrollAnimating}
+      >
+        <Text style={styles.description} numberOfLines={20}>{faker.random.words(200)}</Text>
+        <Catalog onViewMore={this.showFullCatalog} />
+        <HorizontalBookList title="Cùng tác giả" />
+        <View style={{ height: HEADER_MAX_HEIGHT }} />
+      </Animated.ScrollView>
+    );
+  };
 
   render() {
     return (
       <TabView
         style={styles.container}
         navigationState={this.state}
-        renderScene={this._renderScene}
-        renderTabBar={this._renderHeader}
-        onIndexChange={this._handleIndexChange}
+        renderScene={this.renderScene}
+        renderTabBar={this.renderHeader}
+        onIndexChange={this.handleIndexChange}
         initialLayout={initialLayout}
       />
     );
@@ -111,13 +119,19 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     zIndex: 1,
+    backgroundColor: 'white',
   },
   tabBar: {
     backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
   tabBarLabel: {
-    fontSize: 14,
-    color: 'black'
+    fontSize: 13,
+    color: 'black',
+    textTransform: 'capitalize'
   },
   description: {
     lineHeight: 23,
